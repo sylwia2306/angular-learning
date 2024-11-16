7b638a81ed7b2ca0cc1c22ca4ba68e067a546b4c
Sylwia <sylwiaszymanska2306@gmail.com>
2024-11-16 18:33:04 +0100

    content component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,15 @@ import { AppComponent } from "./app.component";
 import { BrowserModule } from "@angular/platform-browser";
 import { NavbarComponent } from "./navbar/navbar.component";
 import { FooterComponent } from "./footer/footer.component";
+import { ContentComponent } from "./content/content.component";
 
 @NgModule({
-    declarations: [AppComponent, NavbarComponent, FooterComponent],
+    declarations: [
+      AppComponent, 
+      NavbarComponent, 
+      FooterComponent, 
+      ContentComponent
+    ],
     imports: [BrowserModule],
     providers: [],
     bootstrap: [AppComponent],
